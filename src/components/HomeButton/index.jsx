77f8e34328a98c86286dc33e9e48be1348fc7469
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-07-01 15:25:39 -0400

    feat: creating home button component

---
+++ src/components/HomeButton/index.jsx
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components'
+import { colorpalette } from '../../commons/colors'
+
+export const HomeButton = styled.button`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    width: 180px;
+    height: 70px;
+    background-color: ${colorpalette.primary};
+    border-radius: 8px;
+    color: ${colorpalette.common.white};
+    font-family: 'Roboto', sans-serif;
+    font-size: 18px;
+    font-weight: bold;    
+`
