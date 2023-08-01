23852b5d2a760f46ff63c66cebad4f583d812883
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-08-01 15:51:20 -0400

    feat: confimation dialog finish

--- src/components/ConfirmationDialog/index.jsx
+++ src/components/ConfirmationDialog/index.jsx
@@ -4,19 +4,43 @@ import { Title } from '../Title'
 import { SimpleButton } from '../SimpleButton'
 import './styles.css'
 import { Text } from '../Text'
+import { Button, Box } from '@mui/material'
 
 function ConfirmationDialog({ open, handleClose }) {
   return (
-    <CustomModal open={open} handleClose={handleClose}>
-      <div id='confirmation-title'>
-        <Title variant='h1'>Confirmação de Entrega</Title>
-        <Text fontSize='18px'>Aguardando confirmação de entrega na área de picking.</Text>
-      </div>
-      <div id='confirmation-button'>
-        <SimpleButton variant='filled' color='primary' onClick={handleClose}>
-          Confirmar Entrega
-        </SimpleButton>
-      </div>
+    <CustomModal open={open}>
+      <Box component='div' sx={{ display: 'flex', flexDirection: 'column', gap: '30px' }}>
+        <div id='confirmation-title'>
+          <Title variant='h1'>Confirmação de Entrega</Title>
+          <Text fontSize='18px'>Aguardando confirmação de entrega na área de picking.</Text>
+        </div>
+        <div id='confirmation-button'>
+          <Button
+            sx={{
+              height: '60px',
+              width: '300px',
+              fontFamily: "'Roboto', sans-serif",
+              fontSize: '18px',
+            }}
+            variant='outlined'
+            color='error'
+            onClick={handleClose}
+          >
+            Cancelar
+          </Button>
+          <Button
+            sx={{
+              height: '60px',
+              width: '300px',
+              fontFamily: "'Roboto', sans-serif",
+              fontSize: '18px',
+            }}
+            variant='contained'
+          >
+            Confirmar Entrega
+          </Button>
+        </div>
+      </Box>
     </CustomModal>
   )
 }
